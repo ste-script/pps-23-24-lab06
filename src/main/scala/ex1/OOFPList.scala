635312306d7332639f08170de058fc7c090ffeca
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-04-15 11:55:35 +0200

    done 1 and 2. Struggling with 3

--- src/main/scala/ex1/OOFPList.scala
+++ src/main/scala/ex1/OOFPList.scala
@@ -7,28 +7,28 @@ enum List[A]:
   def ::(h: A): List[A] = List.::(h, this)
 
   def head: Option[A] = this match
-    case h :: t => Some(h)  // pattern for scala.Option
-    case _ => None          // pattern for scala.Option
+    case h :: t => Some(h) // pattern for scala.Option
+    case _      => None // pattern for scala.Option
 
   def tail: Option[List[A]] = this match
     case h :: t => Some(t)
-    case _ => None
+    case _      => None
   def foreach(consumer: A => Unit): Unit = this match
     case h :: t => consumer(h); t.foreach(consumer)
-    case _ =>
+    case _      =>
 
   def get(pos: Int): Option[A] = this match
     case h :: t if pos == 0 => Some(h)
-    case h :: t if pos > 0 => t.get(pos - 1)
-    case _ => None
+    case h :: t if pos > 0  => t.get(pos - 1)
+    case _                  => None
 
   def foldLeft[B](init: B)(op: (B, A) => B): B = this match
     case h :: t => t.foldLeft(op(init, h))(op)
-    case _ => init
+    case _      => init
 
   def foldRight[B](init: B)(op: (A, B) => B): B = this match
     case h :: t => op(h, t.foldRight(init)(op))
-    case _ => init
+    case _      => init
 
   def append(list: List[A]): List[A] =
     foldRight(list)(_ :: _)
@@ -36,18 +36,24 @@ enum List[A]:
   def flatMap[B](f: A => List[B]): List[B] =
     foldRight(Nil())(f(_) append _)
 
-  def filter(predicate: A => Boolean): List[A] = flatMap(a => if predicate(a) then a :: Nil() else Nil())
+  def filter(predicate: A => Boolean): List[A] =
+    flatMap(a => if predicate(a) then a :: Nil() else Nil())
 
   def map[B](fun: A => B): List[B] = flatMap(a => fun(a) :: Nil())
 
   def reduce(op: (A, A) => A): A = this match
-    case Nil() => throw new IllegalStateException()
+    case Nil()  => throw new IllegalStateException()
     case h :: t => t.foldLeft(h)(op)
 
   // Exercise: implement the following methods
-  def zipWithValue[B](value: B): List[(A, B)] = ???
-  def length(): Int = ???
-  def zipWithIndex: List[(A, Int)] = ???
+
+  def zipWithValue[B](value: B): List[(A, B)] = map((_, value))
+
+  def length(): Int = foldLeft(0)((b, a) => b + 1)
+
+  def zipWithIndex: List[(A, Int)] =
+    foldRight((Nil(), this.length()))((a, b) => (a, b._2 - 1) :: (b._1, b._2))
+
   def partition(predicate: A => Boolean): (List[A], List[A]) = ???
   def span(predicate: A => Boolean): (List[A], List[A]) = ???
   def takeRight(n: Int): List[A] = ???
@@ -67,12 +73,16 @@ object Test extends App:
 
   import List.*
   val reference = List(1, 2, 3, 4)
-  println(reference.zipWithValue(10)) // List((1, 10), (2, 10), (3, 10), (4, 10))
+  println(
+    reference.zipWithValue(10)
+  ) // List((1, 10), (2, 10), (3, 10), (4, 10))
+  println(reference.length())
   println(reference.zipWithIndex) // List((1, 0), (2, 1), (3, 2), (4, 3))
-  println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
+  /*println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
   println(reference.span(_ % 2 != 0)) // (List(1), List(2, 3, 4))
   println(reference.span(_ < 3)) // (List(1, 2), List(3, 4))
   println(reference.reduce(_ + _)) // 10
   println(List(10).reduce(_ + _)) // 10
   println(reference.takeRight(3)) // List(2, 3, 4)
   println(reference.collect { case x if x % 2 == 0 => x + 1 }) // List(3, 5)
+   */
